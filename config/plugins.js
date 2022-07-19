8735fe68e892dbe76cddc747feb49d71d4d0bbef
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-07-19 21:56:52 +0200

    Update env vars

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   "netlify-deployments": {
     enabled: true,
-    buildHook: env("NETLIFY_BUILD_HOOK"),
-    accessToken: env("NETLIFY_BUILD_KEY"),
-    siteId: env("NETLIFY_SITE_ID"),
+    buildHook: process.env.NETLIFY_BUILD_HOOK,
+    accessToken: process.env.NETLIFY_BUILD_KEY,
+    siteId: process.env.NETLIFY_SITE_ID,
   },
   upload: {
     config: {
       provider: "cloudinary",
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
       },
       actionOptions: {
         upload: {},
