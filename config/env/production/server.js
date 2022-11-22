92ba5aacc1c454b939d7c788d6d79587c7c2ab1c
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-11-22 13:58:46 +0100

    Upgrade strapi 4.5.1

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("https://catherinelapsy-backend.herokuapp.com/"),
+  url: env("https://catherinelapsy-backend.up.railway.app/"),
   app: {
     keys: env.array("APP_KEYS"),
   },
